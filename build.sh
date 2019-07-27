08476aa68036d567a215f588ac572655b3b62698
h1x <h1x@linuxconsole.net>
2019-07-27 09:22:53 +0100

    update build script to generate all the files

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
-cat html/html.head.txt > build/index.html
-cmark links/index.links.md >> build/index.html
-echo '</div>' >> build/index.html
-cmark content/index.md >> build/index.html
-echo '</body>' >> build/index.html
+#!/bin/bash 
 
+set -euo pipefail
+
+
+for content_file in content/*; do
+	
+	name=$(basename $content_file .md)
+	build_file="build/$name.html"
+
+	if [ -f links/$name.links.md ]; then
+		links_file="links/$name.links.md"
+	else
+		links_file='links/back.links.md'
+	fi
+
+	cat html/html.head.txt > $build_file
+	cmark $links_file >> $build_file
+	echo '</div>' >> $build_file
+	cmark $content_file >> $build_file
+	echo '</body>' >> $build_file
+
+done
