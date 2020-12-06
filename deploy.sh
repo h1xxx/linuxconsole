aac7f212a953a3bf539025bc42059e2d94732918
h1x <h1x@linuxconsole.net>
2020-12-06 06:24:31 +0100

    add compression benchmarks

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,16 @@ rsync -avP --checksum \
 	_build/ root@www://var/www/linuxconsole.net/
 
 ssh root@www chown -c -R root:nginx /var/www/linuxconsole.net
+
 ssh root@www chmod -c 750 /var/www/linuxconsole.net
-ssh root@www chmod -c 640 /var/www/linuxconsole.net/*
+ssh root@www chmod -c 640 /var/www/linuxconsole.net/*.*
+
 ssh root@www chmod -c 755 /var/www/linuxconsole.net/files
-ssh root@www chmod -c 755 /var/www/linuxconsole.net/benchmarks
 ssh root@www chmod -c 644 /var/www/linuxconsole.net/files/*
-ssh root@www chmod -c 644 /var/www/linuxconsole.net/benchmarks/*
+
+ssh root@www chmod -c 755 /var/www/linuxconsole.net/benchmarks
+ssh root@www chmod -c 755 /var/www/linuxconsole.net/benchmarks/*
+ssh root@www chmod -c 644 /var/www/linuxconsole.net/benchmarks/compression/*.*
+ssh root@www chmod -c 755 /var/www/linuxconsole.net/benchmarks/compression/results_raw_pigz-11
 
 exit 0
